2b5f26c20638358806a0464f5f3d64e3e6717f94
YonLiud <ejliud@gmail.com>
2022-10-21 15:54:59 +0300

    feat: :sparkles: Add the Encryption part

--- src/encrypt.py
+++ src/encrypt.py
@@ -4,6 +4,11 @@ from click import prompt
 from cryptography.fernet import Fernet
 from make_tarfile import make_tarfile as mt
 
+fkey = None
+
+if len(sys.argv) > 2:
+    fkey = open(sys.argv[2], 'rb').read() # read key from file
+
 path = sys.argv[1] # path to target folder
 
 print(os.listdir(path)) # list contents of target folder
@@ -16,3 +21,29 @@ if not prompt('Encrypt files in ' + path + '?', default='y', show_default=True, 
 print('Compresing files...')
 
 mt = mt(path + '.tar.gz', path) # create tar.gz archive of target folder
+
+print('Encrypting files...')
+
+if fkey == None:
+    fkey = Fernet(Fernet.generate_key())
+else:
+    fkey = Fernet(fkey)
+
+with open(path + '.tar.gz', 'rb') as f:
+    data = f.read()
+    
+encrypted = fkey.encrypt(data)
+
+with open(path + '.tar.gz', 'wb') as f:
+    f.write(encrypted)
+
+# delete original folder
+os.rmdir(path)
+
+# save key to file
+with open(path + '.key', 'wb') as f:
+    f.write(str(fkey).encode())
+
+f.close()
+
+print('Done! \nEncrypted files are in ' + path + '.tar.gz')
