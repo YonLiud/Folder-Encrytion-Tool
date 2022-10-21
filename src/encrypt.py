2fb97e2eda47af26eea2c67adf91aa758c8f3796
YonLiud <ejliud@gmail.com>
2022-10-21 15:33:19 +0300

    feat: :sparkles: Add make_tarfile
    Add a function to create a tar file from the target's folder

--- src/encrypt.py
+++ src/encrypt.py
@@ -1,7 +1,18 @@
 import sys
 import os
+from click import prompt
+from cryptography.fernet import Fernet
+from make_tarfile import make_tarfile as mt
 
 path = sys.argv[1] # path to target folder
 
 print(os.listdir(path)) # list contents of target folder
 
+# prompt confirmation to encrypt
+if not prompt('Encrypt files in ' + path + '?', default='y', show_default=True, type=bool):
+    print('Aborting...')
+    exit()
+
+print('Compresing files...')
+
+mt = mt(path + '.tar.gz', path) # create tar.gz archive of target folder
