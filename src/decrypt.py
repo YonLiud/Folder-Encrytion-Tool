45ef4037835f1585bfd8765c9ec357496b7266d2
YonLiud <ejliud@gmail.com>
2022-11-03 18:57:50 +0200

    feat: :sparkles: Add decrypt to main
    Add decrypt function to main.py for later UI development

--- src/decrypt.py
+++ src/decrypt.py
@@ -4,39 +4,30 @@ from cryptography.fernet import Fernet
 from numpy import extract
 from extract_tarfile import extract_tarfile as et
 
-print('Decrypting files...')
-
 cmdargs = sys.argv # cmd arguments ( path to target folder, path to key file )
 
-try:
-    fkey = open(cmdargs[2], 'rb').read() # read key from file
-except IndexError:
-    print('No key file specified. Aborting...')
-    exit()
-except Exception as e:
-    print('Unable to read key file: ' + str(e))
-    exit()
-fkey = Fernet(fkey) # create Fernet object
-path = cmdargs[1] # path to target file
+def decrypt(path, keypath):
+    fkey = open(keypath, 'rb').read() # read key from file
+    fkey = Fernet(fkey)
 
-with open(path, 'rb') as f:
-    data = f.read()
+    with open(path, 'rb') as f:
+        data = f.read()
 
-decrypted = fkey.decrypt(data)
+    decrypted = fkey.decrypt(data)
 
-with open(path, 'wb') as f:
-    f.write(decrypted)
+    with open(path, 'wb') as f:
+        f.write(decrypted)
 
-print('Decompressing files...')
+    print('Decompressing files...')
 
-et(path)
+    et(path)
 
-print('Done! \nDecrypted files are in ' + path[:-7])
+    # delete archive
+    os.remove(path)
 
-# delete archive
-os.remove(path)
-
-# remove .secured suffix from folder name
-fname = path.split('\\')[-1]
-new_name = fname[:-8]
-os.rename(path[:-7], path[:-7][:-8])
+    # remove .secured suffix from folder name
+    fname = path.split('\\')[-1]
+    new_name = fname[:-8]
+    os.rename(path[:-7], path[:-7][:-8])
+    
+    print('\nDecrypted files are in ' + path[:-7])
--- src/main.py
+++ src/main.py
@@ -1,18 +1,42 @@
 from encrypt import encrypt
+from decrypt import decrypt
 import os
 from click import prompt
 
+opt = prompt("Encrypt or Decrypt?", type=str)
 
-path = input("Insert Directory Path: ")
-key = input("Insert Key Path: ")
+if opt == "Encrypt":
+    path = input("Insert Directory Path: ")
+    key = input("Insert Key Path: ")
 
-if key == "":
-    key = None
+    if key == "":
+        key = None
 
-print(os.listdir(path)) # list contents of target folder
+    print(os.listdir(path)) # list contents of target folder
 
-if not prompt('Encrypt files in ' + path + '?', default='y', show_default=True, type=bool):
-    print('Aborting...')
+    if not prompt('Encrypt files in ' + path + '?', default='y', show_default=True, type=bool):
+        print('Aborting...')
+        exit()
+
+    encrypt(path)
+elif opt == "Decrypt":
+    path = input("Insert Directory Path: ")
+    key = input("Insert Key Path: ")
+
+    if key == "":
+        print("No key provided, using default key")
+        key = os.path.basename(path)
+        key = key[:-15]
+        key = key + ".key"
+        # locate key in same directory as encrypted file
+        key = os.path.join(os.path.dirname(path), key)
+        if not os.path.isfile(key):
+            print("No key found, aborting...")
+            exit()
+        print("Using key: " + key)
+    decrypt(path, key)
+    
+else:
+    print("Invalid option")
+
     exit()
-
-encrypt(path)
