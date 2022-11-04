c2bd26772de2629214f76deeeb3559af9c1266c5
YonLiud <ejliud@gmail.com>
2022-11-04 13:22:48 +0200

    feat: :sparkles: Add Tkinter
    Add Tkinter GUI to the project

--- src/main.py
+++ src/main.py
@@ -1,42 +1,96 @@
 from encrypt import encrypt
 from decrypt import decrypt
 import os
-from click import prompt
 
-opt = prompt("Encrypt or Decrypt?", type=str)
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 
-if opt == "Encrypt":
-    path = input("Insert Directory Path: ")
-    key = input("Insert Key Path: ")
-
-    if key == "":
-        key = None
-
-    print(os.listdir(path)) # list contents of target folder
-
-    if not prompt('Encrypt files in ' + path + '?', default='y', show_default=True, type=bool):
-        print('Aborting...')
-        exit()
-
-    encrypt(path)
-elif opt == "Decrypt":
-    path = input("Insert Directory Path: ")
-    key = input("Insert Key Path: ")
-
-    if key == "":
-        print("No key provided, using default key")
-        key = os.path.basename(path)
-        key = key[:-15]
-        key = key + ".key"
-        # locate key in same directory as encrypted file
-        key = os.path.join(os.path.dirname(path), key)
-        if not os.path.isfile(key):
-            print("No key found, aborting...")
-            exit()
-        print("Using key: " + key)
-    decrypt(path, key)
+def main():
+    # basic tkinter window
+    window = tk.Tk()
+    window.minsize(500, 300)
+    # open window in center of screen
     
-else:
-    print("Invalid option")
+    window.title('Folder Encryption Tool')
 
-    exit()
+    
+    
+    def encrypt_folder():
+        
+        window.withdraw()
+        
+        encwindow = tk.Toplevel()
+        encwindow.minsize(500, 300)
+        encwindow.geometry("500x300+{}+{}".format(int(window.winfo_screenwidth()/2-250), int(window.winfo_screenheight()/2-150)))
+        
+        while True:
+            folder = filedialog.askdirectory()
+            
+            if folder == '':
+                messagebox.showerror('Error, No folder specified', 'No folder selected, a folder must be selected to encrypt')
+                encwindow.destroy()
+                window.deiconify()
+            else:
+                break
+        tk.Label(encwindow, text='Selected folder: ' + folder).pack()   
+        
+        while True:
+            option = None
+            key = filedialog.askopenfilename(filetypes=[('Key files', '*.key')])
+            tk.Label(encwindow, text="Selected key file: " + str(key)).pack()
+
+            if key == '':
+                messagebox.showwarning('No key selected', 'No key has been selected')
+                newkey = messagebox.askyesno('No key selected', 'No key selected, would you like to create a new key?')
+                if newkey == True:
+                    messagebox.showinfo('New key', 'A new key will be created')
+                    key = None
+                    break
+    
+        
+        encrypt(folder, key)
+        encwindow.destroy()
+        window.deiconify()
+
+    def decrypt_folder():
+        window.withdraw()
+        
+        decwindow = tk.Toplevel()
+        decwindow.minsize(500, 300)
+        decwindow.geometry("500x300+{}+{}".format(int(window.winfo_screenwidth()/2-250), int(window.winfo_screenheight()/2-150)))
+        
+        while True:
+            folder = filedialog.askopenfilename(filetypes=[('Encrypted Folder', '*.secured.tar.gz')])
+            
+            if folder == '':
+                messagebox.showerror('Error, No folder specified', 'No folder selected, a folder must be selected to decrypt')
+                decwindow.destroy()
+                window.deiconify()
+            else:
+                break
+        tk.Label(decwindow, text='Selected folder: ' + folder).pack()   
+        
+        while True:
+            option = None
+            key = filedialog.askopenfilename(filetypes=[('Key files', '*.key')])
+            tk.Label(decwindow, text="Selected key file: " + str(key)).pack()
+
+            if key == '':
+                messagebox.showwarning('No key selected', 'A key must be selected to decrypt')
+            else:
+                break
+        
+        
+        decrypt(folder, key)
+        decwindow.destroy()
+        window.deiconify()
+        
+        
+    tk.Button(window, text='Encrypt Folder', command=encrypt_folder).pack()
+    tk.Button(window, text='Decrypt Folder', command=decrypt_folder).pack()
+    
+    window.mainloop()
+    
+if __name__ == '__main__':
+    main()
